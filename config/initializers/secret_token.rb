f017e15663b06d58652e08a368bdc03b98f99eac
mehdijalali <mehdijalali.co@gmail.com>
2013-09-16 18:27:46 +0430

    Initialze repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+JewellryProject::Application.config.secret_key_base = '2ab458fb2e8480fbac0dc81221affb11242e6c9002d9c518ad9f77bc6a6b7c8d724ba30681eeee84111588b9262e17062482b206d8e420f3ddbd40d8f576c7ae'
